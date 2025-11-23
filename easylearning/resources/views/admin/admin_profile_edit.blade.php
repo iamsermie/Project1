928dcca2a5cc0af8f0a2851cf9b903e0328cfe94
Samuel Olayiwola <iamsermie@icloud.com>
2025-11-23 14:02:50 +0000

    Image upload live update

--- easylearning/resources/views/admin/admin_profile_edit.blade.php
+++ easylearning/resources/views/admin/admin_profile_edit.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <div class="page-content">
                     <div class="container-fluid">
                         <div class="row">
@@ -35,7 +35,7 @@
                                         <div class="row mb-3">
                                             <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                                             <div class="col-sm-10">
-                                                <input class="form-control" name="profile_image" id="profile_image" type="file" placeholder=""  id="example-text-input">
+                                                <input class="form-control" name="profile_image" id="image" type="file" placeholder="">
                                             </div>
                                         </div>
 
@@ -43,7 +43,7 @@
                                             <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                                             <div class="col-sm-10">
 
-                                                <img class="rounded avatar-lg" src="{{ asset ('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                                                <img id="showImage" class="rounded avatar-lg" src="{{ asset ('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
                                             </div>
                                         </div>
                                         <input type="submit" class="btn btn-info waves-effect waves-light" value="Update Profile">
@@ -61,6 +61,16 @@
                     </div>
                     </div>
 
-
+<script>
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
 
 @endsection
